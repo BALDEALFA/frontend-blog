7e5572241bd74e0283d4489f9f38ccbeb4d1c210
BALDEALFA <alfaafro@gmail.com>
2021-02-27 10:49:08 +0000

    Deleting_documents

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -5,6 +5,7 @@
 	<div v-if="post" class="post">
 		<h3>{{ post.title }}</h3>
 		<p class="pre">{{ post.body }}</p>
+		<button @click="handleClick" class="delete">delete post</button>
 	</div>
 	<div v-else>
 		<Spinner />
@@ -14,14 +15,25 @@
 <script>
 import getPost from "../composables/getPost";
 import Spinner from "../components/Spinner.vue";
+import { projectFirestore } from "../firebase/config";
+import { useRoute, useRouter } from "vue-router";
 
 export default {
 	props: ["id"],
 	components: { Spinner },
 	setup(props) {
+		const router = useRouter();
 		const { post, error, load } = getPost(props.id);
 		load();
-		return { post, error };
+
+		const handleClick = async () => {
+			await projectFirestore
+				.collection("posts")
+				.doc(props.id)
+				.delete();
+			router.push({ name: "Home" });
+		};
+		return { post, error, handleClick };
 	},
 };
 </script>
@@ -38,5 +50,8 @@ export default {
 }
 .pre {
 	white-space: pre-wrap;
+}
+button.delete {
+	margin: 10px auto;
 }
 </style>
